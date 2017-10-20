4254ce84f898cde8632e4825da84bd40c1fc740d
jhua182 <jhua182@student.monash.edu>
2017-10-20 16:41:34 +1100

    Add files via upload

---
+++ Emailaddress.py
@@ -0,0 +1,8 @@
+class Email:
+    def __init__(self):
+        self.emailaddress = None
+
+    def setEmailAddress(self,emailaddress):
+        self.emailaddress= emailaddress
+    def getEmailAddress(self):
+        return self.emailaddress
